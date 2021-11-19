4f5fd22dbb5d286a217e3153f9afccd1b74039de
guypeleg25 <46633558+guypeleg25@users.noreply.github.com>
2021-11-19 15:23:54 +0200

    init 3 ms

--- EurekaServer/demo/src/main/java/com/example/demo/EurekaServerApplication.java
+++ EurekaServer/demo/src/main/java/com/example/demo/EurekaServerApplication.java
@@ -8,6 +8,7 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+		System.out.println("EurekaServer started");
 	}
 
 }
--- ServiceShuffle/demo/src/main/java/com/example/demo/ServiceShuffleApplication.java
+++ ServiceShuffle/demo/src/main/java/com/example/demo/ServiceShuffleApplication.java
@@ -8,6 +8,7 @@ public class ServiceShuffleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceShuffleApplication.class, args);
+		System.out.println("ServiceShuffle started");
 	}
 
 }
